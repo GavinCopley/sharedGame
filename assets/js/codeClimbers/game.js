8fe7d168bb926418cbbd833f11730529938ead75
GavinCopley <gbcopley@gmail.com>
2023-10-21 02:39:41 +0000

    fixing the platform image

--- assets/js/codeClimbers/game.js
+++ assets/js/codeClimbers/game.js
@@ -1,8 +1,11 @@
+import platform from "/{{site.baseurl}}/images/road.png"
+console.log("platform")
+
 const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = 650;
+canvas.height = 400;
 
 const gravity = 1.5
 
@@ -38,10 +41,10 @@ class Player {
 }
 
 class Platform {
-    constructor() {
+    constructor({ x, y }) {
         this.position = {
-            x: 200,
-            y: 100
+            x,
+            y
         }
         this.width = 200
         this.height = 20
@@ -54,7 +57,11 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({
+    x: 200, y:100
+}), new Platform({
+    x: 500,  y:200
+})]
 
 const keys = {
     right: {
@@ -65,20 +72,55 @@ const keys = {
     }
 }
 
+let scrollOffset = 0
+
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platform.draw()
+    platforms.forEach((platform) => {
+        platform.draw()
+    })
 
-    if (keys.right.pressed) {
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5
-    } else player.velocity.x = 0
+    } else {
+    player.velocity.x = 0
+        
+        if (keys.right.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x -= 5
+                scrollOffset += 5
+            })
+            console.log("rightplat")
+        } else if (keys.left.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x += 5
+                scrollOffset -= 5
+            })
+            console.log("leftplat")
+        }
+    }
 
-    if (player.position.y + player.height <= platform.position.y) {
-        player.velocity.y = 0
+    console.log(scrollOffset)
+
+    //platform collision
+    platforms.forEach((platform) => {
+        if (
+            player.position.y + player.height <= platform.position.y && 
+            player.position.y + player.height + player.velocity.y >= platform.position.y && 
+            player.position.x + player.width >= platform.position.x && 
+            player.position.x <= platform.position.x + platform.width
+        ){
+            player.velocity.y = 0
+            console.log("stop")
+        }
+    })
+
+    if (scrollOffset > 2000) {
+        console.log("win")
     }
 }
 
@@ -103,7 +145,7 @@ addEventListener("keydown", ({ keyCode }) => {
         
         case 87:
             console.log("up")
-            player.velocity.y -= 10
+            player.velocity.y -= 20
             break
     }
 })
